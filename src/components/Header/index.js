5d3cde142ed295e0b9b56750a67cf33198ce5e7a
mateusedua <mateuseduardoteuta10@gmail.com>
2023-07-07 00:43:39 +0000

    adicionando nav no header e menu do usuario

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,43 +1,42 @@
 import * as Styles from './style'
 import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 import { logoutUser } from '../../redux/user/userSlice'
-
+import { useState } from 'react'
+import { AiFillCaretDown } from 'react-icons/ai'
 
 const Header = () => {
 
     const dispatch = useDispatch()
     const history = useHistory()
+    const [isOpen, setIsOpen] = useState(false)
 
     const handleSair = () => {
         dispatch(logoutUser())
-    }
-
-    const handlePerfil = () => {
-        history.push({
-            pathname: "/Pefil"
-        })
-    }
-
-    const handleHome = () => {
-        history.push({
-            pathname: "/"
-        })
     }
 
     const { user } = useSelector(state => state.userReducer)
 
     return(
         <Styles.Container>
-            <Styles.Button onClick={handleSair}>
-                Sair
-            </Styles.Button>
-            <Styles.Button onClick={handlePerfil}>
-                {user.nome}
-            </Styles.Button>
-            <Styles.Button onClick={handleHome}>
-                Home
-            </Styles.Button>
+            <Styles.Nav>
+
+                <Styles.LinkHeader size={'25px'} to="/">Home</Styles.LinkHeader>
+                <div>
+
+                    <Styles.DropDown onClick={(e) => setIsOpen(!isOpen)}>
+                        {user.nome}
+                        <AiFillCaretDown width={20} height={20} />
+                    </Styles.DropDown>
+                    {
+                        isOpen &&
+                        <Styles.ItemDown>
+                            <Styles.LinkHeader to="/Perfil" onClick={(e) => setIsOpen(!isOpen)}>Perfil</Styles.LinkHeader>
+                            <Styles.Sair onClick={handleSair}>Sair</Styles.Sair>
+                        </Styles.ItemDown>
+                    }
+                </div>
+            </Styles.Nav>
         </Styles.Container>
     )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
     width: 100%;
@@ -26,10 +27,63 @@ export const Button = styled.button`
         background-color: #83B7E5;
     }
 `
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: flex-end;
+    gap: 10px;
+`
+
+export const DropDown = styled.div`
+    font-size: 25px;
+    font-weight: 700;
+    padding: 7px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    cursor: pointer;
+
+    &:hover{
+        text-decoration: underline;
+    }
+    
+`
+
+export const ItemDown = styled.div`
+    position: absolute;
+    background-color: white;
+    width: 130px;
+    font-weight: 700;
+    font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 7px;
+    padding: 5px;
+`
+export const Sair = styled.span`
+    cursor: pointer;
+    
+    &:hover{
+        text-decoration: underline;
+    }
+`
+
+export const LinkHeader = styled(Link)`
+    text-decoration: none;
+    color: black;
+    font-weight: 700;
+    font-size: ${props => props.size ? props.size : null};
+    &:hover{
+        text-decoration: underline;
+    }
+`
 
 export const Text = styled.span`
     color: black;
     font-size: 25px;
     font-weight: bold;
     margin-left: 10px;
+    
 `
