d4dbbebf64e24bb5133daf4a1bf17277f34322b4
mateusedua <mateuseduardoteuta10@gmail.com>
2023-07-21 23:03:14 +0000

    alterando cor do background

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,5 +1,5 @@
 export default {
-    backgroudColor: '#F6F5FC',
+    backgroudColor: '#DFE6DE',
     size_tablet: 'and (min-width:769px) and (max-width: 1024px)',
     size_mobile: 'and (max-width: 768px)'
 }
